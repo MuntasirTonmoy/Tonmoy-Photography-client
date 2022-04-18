bcbb02cc252732941b017b32837c32ea9f0df50a
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-04-18 16:59:51 +0600

    nav-bar half complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AboutMe from "./Pages/AboutMe/AboutMe";
+import Blog from "./Pages/Blog/Blog";
 import Home from "./Pages/Home/Home";
 import SignIn from "./Pages/SignIn/SignIn";
+import SignUp from "./Pages/SignUp/SignUp";
 import Footer from "./Shared/Footer/Footer";
 import Header from "./Shared/Header/Header";
 import NotFound from "./Shared/NotFound/NotFound";
@@ -12,7 +16,11 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="/aboutMe" element={<AboutMe></AboutMe>}></Route>
+
         <Route path="/signIn" element={<SignIn></SignIn>}></Route>
+        <Route path="/signUp" element={<SignUp></SignUp>}></Route>
         <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -1,14 +1,24 @@
 import React from "react";
+import "./Header.css";
+import logo from "../../logo.gif";
 import { Link } from "react-router-dom";
+import CustomLink from "../CustomLink/CustomLink";
 
 const Header = () => {
   return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/signIn">SignIn</Link>
-      </nav>
-    </div>
+    <nav className="nav-bar">
+      <h1 className="logo-text">
+        <img src={logo} alt="" /> Tonmoy Photography
+      </h1>
+
+      <div className="nav-menu">
+        <CustomLink to="/">Home</CustomLink>
+        <CustomLink to="/blog">Blog</CustomLink>
+        <CustomLink to="/aboutMe">About Me</CustomLink>
+        <CustomLink to="/signIn">SignIn</CustomLink>
+        <CustomLink to="/signUp">SignUp</CustomLink>
+      </div>
+    </nav>
   );
 };
 
